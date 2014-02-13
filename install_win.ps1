b9402edcdf702f1d7e33c20a82472784b9fcf290
vorou <vorou@skbkontur.ru>
2014-02-14 00:02:29 +0600

    remove .mongorc.js on installation

--- install_win.ps1
+++ install_win.ps1
@@ -1,4 +1,7 @@
 $modules = @(ls .\base.js) + @(ls .\config.js) + @(ls .\hacks\*.js)
 cat $modules | out-file -encoding ASCII mongo_hacker.js 
 cp -force .\mongo_hacker.js ~
-add-content ~\.mongorc.js "`r`nload('mongo_hacker.js')"
+rm ~\.mongorc.js
+$hacker = (resolve-path .\mongo_hacker.js).tostring()
+$hacker = $hacker.replace("\", "/")
+add-content ~\.mongorc.js "`r`nload('$hacker')"
